14b013233202d46bc025c7d94794ce2c0100a1e7
Xavier Guérin <xavier.guerin@imag.fr>
2009-09-09 17:44:30 +0200

    apes-build now default to the component in PWD when no args are passed.

--- Tools/Ruby/lib/apes/component.rb
+++ Tools/Ruby/lib/apes/component.rb
@@ -37,145 +37,149 @@ class APEComponent
     # Get the data
     #
 
-    xml_data = File.new(file_path + "/component.xml")
-    file = REXML::Document.new(xml_data)
-    root = file.root
-    author = root.attributes["author"]
-    unique = root.attributes["unique"] == "true"
-    wrapper = root.attributes["wrapper"] == "true"
-  
-    #
-    # Get the component's ID
-    #
-    
-    ids = []
-    root.elements.each("id") do |i|
-      ids << APEId.createFromXML(i)
-    end
+    begin
+      xml_data = File.new(file_path + "/component.xml")
+      file = REXML::Document.new(xml_data)
+      root = file.root
+      author = root.attributes["author"]
+      unique = root.attributes["unique"] == "true"
+      wrapper = root.attributes["wrapper"] == "true"
 
-    if ids.length == 0 then
-      abort "[component] no ID found for component at " + file_path
-    elsif ids.length > 1 then
-      abort "[component] too many IDs found for component at " + file_path
-    end
+      #
+      # Get the component's ID
+      #
 
-    component = APEComponent.new(file_path, ids[0], author, unique, wrapper)
-
-    #
-    # Get the injected IDs
-    #
-
-    root.elements.each("inject/id") do |i|
-      id = APEId.createFromXML(i)
-      if component.injected_ids.find { |i| i == id } == nil then
-        component.injected_ids << id
-      else
-        abort "Duplicate injected id " + id.to_s + " in component at " + @path
+      ids = []
+      root.elements.each("id") do |i|
+        ids << APEId.createFromXML(i)
       end
-    end
 
-    #
-    # Get the restricted IDs
-    #
-
-    root.elements.each("restrict/id") do |i|
-      id = APEId.createFromXML(i)
-      if component.restricted_ids.find { |i| i == id } == nil then
-        component.restricted_ids << id
-      else
-        puts "Component at path:"
-        puts "\t" + @path
-        abort "Duplicate restricted id: " + id.to_s
+      if ids.length == 0 then
+        abort "[component] no ID found for component at " + file_path
+      elsif ids.length > 1 then
+        abort "[component] too many IDs found for component at " + file_path
       end
-    end
 
-    #
-    # Get the managed methods
-    #
+      component = APEComponent.new(file_path, ids[0], author, unique, wrapper)
 
-    root.elements.each("manage/method") do |m|
-      item = APEMethod.createFromXML(m)
-      if component.managed_methods.find { |i| i == item } == nil then
-        component.managed_methods << item
-      else
-        puts id.to_s
-        abort "Duplicate managed method:" + item.name
-      end
-    end
+      #
+      # Get the injected IDs
+      #
 
-    #
-    # Get the provided items
-    #
-
-    root.elements.each("provide") do |p|
-      p.elements.each("type") do |t|
-        item = APEType.createFromXML(t)
-        if component.provided_types.find { |i| i == item } == nil then
-          component.provided_types << item
+      root.elements.each("inject/id") do |i|
+        id = APEId.createFromXML(i)
+        if component.injected_ids.find { |i| i == id } == nil then
+          component.injected_ids << id
         else
-          puts item.to_s
-          abort "Duplicate provided type" + item.name
+          abort "Duplicate injected id " + id.to_s + " in component at " + @path
         end
       end
 
-      p.elements.each("definition") do |d|
-        item = APEDefinition.createFromXML(d)
-        if component.provided_definitions.find { |i| i == item } == nil then
-          component.provided_definitions << item
+      #
+      # Get the restricted IDs
+      #
+
+      root.elements.each("restrict/id") do |i|
+        id = APEId.createFromXML(i)
+        if component.restricted_ids.find { |i| i == id } == nil then
+          component.restricted_ids << id
         else
-          puts item.to_s
-          abort "Duplicate provided definition" + item.name
+          puts "Component at path:"
+          puts "\t" + @path
+          abort "Duplicate restricted id: " + id.to_s
         end
       end
 
-      p.elements.each("method") do |m|
+      #
+      # Get the managed methods
+      #
+
+      root.elements.each("manage/method") do |m|
         item = APEMethod.createFromXML(m)
-        if component.provided_methods.find { |i| i == item } == nil then
-          component.provided_methods << item
+        if component.managed_methods.find { |i| i == item } == nil then
+          component.managed_methods << item
         else
-          puts item.to_s
-          abort "Duplicate provided method" + item.name
+          puts id.to_s
+          abort "Duplicate managed method:" + item.name
         end
       end
+
+      #
+      # Get the provided items
+      #
+
+      root.elements.each("provide") do |p|
+        p.elements.each("type") do |t|
+          item = APEType.createFromXML(t)
+          if component.provided_types.find { |i| i == item } == nil then
+            component.provided_types << item
+          else
+            puts item.to_s
+            abort "Duplicate provided type" + item.name
+          end
+        end
+
+        p.elements.each("definition") do |d|
+          item = APEDefinition.createFromXML(d)
+          if component.provided_definitions.find { |i| i == item } == nil then
+            component.provided_definitions << item
+          else
+            puts item.to_s
+            abort "Duplicate provided definition" + item.name
+          end
+        end
+
+        p.elements.each("method") do |m|
+          item = APEMethod.createFromXML(m)
+          if component.provided_methods.find { |i| i == item } == nil then
+            component.provided_methods << item
+          else
+            puts item.to_s
+            abort "Duplicate provided method" + item.name
+          end
+        end
+      end
+
+      #
+      # Get the required items
+      #
+
+      root.elements.each("require") do |p|
+        p.elements.each("type") do |t|
+          item = APEType.createFromXML(t)
+          if component.required_types.find { |i| i == item } == nil then
+            component.required_types << item
+          else
+            puts item.to_s
+            abort "Duplicate required type" + item.name
+          end
+        end
+
+        p.elements.each("definition") do |d|
+          item = APEDefinition.createFromXML(d)
+          if component.required_definitions.find { |i| i == item } == nil then
+            component.required_definitions << item
+          else
+            puts item.to_s
+            abort "Duplicate required definition" + item.name
+          end
+        end
+
+        p.elements.each("method") do |m|
+          item = APEMethod.createFromXML(m)
+          if component.required_methods.find { |i| i == item } == nil then
+            component.required_methods << item
+          else
+            puts item.to_s
+            abort "Duplicate required method" + item.name
+          end
+        end
+      end
+
+      component
+    rescue
+      nil
     end
-
-    #
-    # Get the required items
-    #
-
-    root.elements.each("require") do |p|
-      p.elements.each("type") do |t|
-        item = APEType.createFromXML(t)
-        if component.required_types.find { |i| i == item } == nil then
-          component.required_types << item
-        else
-          puts item.to_s
-          abort "Duplicate required type" + item.name
-        end
-      end
-
-      p.elements.each("definition") do |d|
-        item = APEDefinition.createFromXML(d)
-        if component.required_definitions.find { |i| i == item } == nil then
-          component.required_definitions << item
-        else
-          puts item.to_s
-          abort "Duplicate required definition" + item.name
-        end
-      end
-
-      p.elements.each("method") do |m|
-        item = APEMethod.createFromXML(m)
-        if component.required_methods.find { |i| i == item } == nil then
-          component.required_methods << item
-        else
-          puts item.to_s
-          abort "Duplicate required method" + item.name
-        end
-      end
-    end
-
-    component
   end
 
   def overlap?(component)
