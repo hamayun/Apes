5298bdef01f038c9debd966f35726c385bf26b45
Xavier Guérin <xavier.guerin@imag.fr>
2010-01-15 16:00:07 +0100

    Removed unnecessary RM.

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ export APES_ROOT=$PWD
 export APES_COMPONENT_PATH=$APES_ROOT/Components
 export RUBYLIB=$RUBYLIB:$APES_ROOT/Tools/Ruby/lib
 export PATH=$PATH:$APES_ROOT/Tools/Ruby/bin
+export PATH=$PATH:$APES_ROOT/Tools/Shell/bin
 
 echo 'APES_ROOT = '$APES_ROOT
 echo 'RUBY_LIB += '$APES_ROOT/Tools/Ruby/lib
