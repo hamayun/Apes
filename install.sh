42edbdb793ca84ef32ab1e28a5edabfabe79e0cb
Xavier Guérin <xavier.guerin@imag.fr>
2010-03-29 08:47:02 +0200

    Added a check on the APES_ROOT environment variable.

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-export APES_COMPONENT_PATH=$APES_ROOT/Components
-export RUBYLIB=$RUBYLIB:$APES_ROOT/Tools/Ruby/lib
-export GEM_PATH=$GEM_PATH:$APES_ROOT/Tools/Ruby/Gems
-export PATH=$PATH:$APES_ROOT/Tools/Ruby/bin
-export PATH=$PATH:$APES_ROOT/Tools/Shell/bin
+if [ "x$APES_ROOT" = "x" ] ; then
+  echo "The \$APES_ROOT environment variable is not defined."
+elif [ ! -d $APES_ROOT ] ; then
+  echo "\$APES_ROOT invalid."
+else
+  export APES_COMPONENT_PATH=$APES_ROOT/Components
+  export RUBYLIB=$RUBYLIB:$APES_ROOT/Tools/Ruby/lib
+  export GEM_PATH=$GEM_PATH:$APES_ROOT/Tools/Ruby/Gems
+  export PATH=$PATH:$APES_ROOT/Tools/Ruby/bin
+  export PATH=$PATH:$APES_ROOT/Tools/Shell/bin
+fi 
