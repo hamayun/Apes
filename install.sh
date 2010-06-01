4b268f1e5fc375edee03e4c13e07a7a96d7929ec
Xavier Guérin <xavier.guerin@imag.fr>
2010-06-01 13:22:03 +0200

    Removed GEM directory. Now gems have to be installed manually to keep compatibility with debian.

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ elif [ ! -d $APES_ROOT ] ; then
 else
   export APES_COMPONENT_PATH=$APES_ROOT/Components
   export RUBYLIB=$RUBYLIB:$APES_ROOT/Tools/Ruby/lib
-  export GEM_PATH=$GEM_PATH:$APES_ROOT/Tools/Ruby/Gems
   export PATH=$PATH:$APES_ROOT/Tools/Ruby/bin
   export PATH=$PATH:$APES_ROOT/Tools/Shell/bin
 
