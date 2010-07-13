e1e53dde09633e10960b006be778d84073e9a194
Xavier Guérin <xavier.guerin@imag.fr>
2010-07-13 14:15:45 +0200

    Renamed bin -> Applications and lib -> Library.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/Applications/sh
 
 if [ "x$APES_ROOT" = "x" ] ; then
   echo "The \$APES_ROOT environment variable is not defined."
@@ -6,13 +6,13 @@ elif [ ! -d $APES_ROOT ] ; then
   echo "\$APES_ROOT invalid."
 else
   export APES_PATH=$APES_ROOT/Components
-  export RUBYLIB=$RUBYLIB:$APES_ROOT/Tools/Ruby/lib
-  export PATH=$PATH:$APES_ROOT/Tools/Ruby/bin
-  export PATH=$PATH:$APES_ROOT/Tools/Shell/bin
+  export RUBYLIB=$RUBYLIB:$APES_ROOT/Tools/Ruby/Library
+  export PATH=$PATH:$APES_ROOT/Tools/Ruby/Applications
+  export PATH=$PATH:$APES_ROOT/Tools/Shell/Applications
 
   if [ -e $APES_ROOT/Toolchains ] ; then
     for i in $(\ls $APES_ROOT/Toolchains) ; do
-      export PATH=$APES_ROOT/Toolchains/$i/bin:$PATH
+      export PATH=$APES_ROOT/Toolchains/$i/Applications:$PATH
       export MANPATH=$APES_ROOT/Toolchains/$i/man:$MANPATH
       export INFOPATH=$APES_ROOT/Toolchains/$i/info:$INFOPATH
     done ;
