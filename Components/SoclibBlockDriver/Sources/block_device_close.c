a93861c3dce3bef0bba5e6b80d51f967de82872f
Mian M. Hamayun <mian-muhammad.hamayun@imag.fr>
2011-07-26 19:52:22 +0200

    Removed prints from SoclibBlockDriver Open and Close Functions

--- Components/SoclibBlockDriver/Sources/block_device_close.c
+++ Components/SoclibBlockDriver/Sources/block_device_close.c
@@ -6,7 +6,7 @@
 status_t block_device_close (void * data)
 {
 
-  dna_printf("In block_device_close-------\n");
+  //dna_printf("In block_device_close-------\n");
 
   return DNA_OK ;
 }
--- Components/SoclibBlockDriver/Sources/block_device_open.c
+++ Components/SoclibBlockDriver/Sources/block_device_open.c
@@ -7,7 +7,7 @@ status_t block_device_open (char * name, int32_t mode, void ** data)
 {
   char * num = name + dna_strlen(name) -1;
   
-  dna_printf("In block_device_open -------\n");
+  //dna_printf("In block_device_open -------\n");
 
   block_device_control_t * block_device_p = &(block_device_controls[dna_atoi(num)]) ;
   *data = (void *) block_device_p ;
